2acee022d4409a8a809b7dbcfa14eae2c2f15ffc
shehio <shehabyasser@gmail.com>
2020-05-01 17:28:42 -0700

    Moved asset attribution to its own file.

--- assignments/assignment4/question4.py
+++ assignments/assignment4/question4.py
@@ -1,3 +1,5 @@
+from src.performance.assetattribution import AssetAttribution
+
 import numpy as np
 
 
@@ -10,38 +12,6 @@ def my_print(strings):
 
 def my_round(number):
     return round(number, 4)
-
-
-class AssetAttribution:
-    # Notice that the way this works might not be extremely straightforward.
-    # The returns are expected to be in % while the weights are not.
-    # Will fix this in the next iteration.
-    def __init__(self, portfolio_weight, benchmark_weight, portfolio_return, benchmark_return):
-        self.portfolio_weight = portfolio_weight
-        self.benchmark_weight = benchmark_weight
-        self.portfolio_return = portfolio_return
-        self.benchmark_return = benchmark_return
-
-    def get_allocation(self, _benchmark_total_return):
-        return (self.portfolio_weight - self.benchmark_weight) * (self.benchmark_return - _benchmark_total_return)
-
-    def get_selection(self):
-        return self.benchmark_weight * (self.portfolio_return - self.benchmark_return)
-
-    def get_interaction(self):
-        return (self.portfolio_weight - self.benchmark_weight) * (self.portfolio_return - self.benchmark_return)
-
-    def get_top_down_allocation(self, _benchmark_total_return):
-        return self.get_allocation(_benchmark_total_return)
-
-    def get_bottom_up_allocation(self, _benchmark_total_return):
-        return (self.portfolio_weight - self.benchmark_weight) * (self.portfolio_return - _benchmark_total_return)
-
-    def get_top_down_selection(self):
-        return self.portfolio_weight * (self.portfolio_return - self.benchmark_return)
-
-    def get_bottom_up_selection(self):
-        return self.get_selection()
 
 # Q4.
 # Asset class   Portfolio weight    Benchmark weight    Portfolio return    Benchmark return
