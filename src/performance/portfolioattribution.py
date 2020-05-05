928374a07f3aba9fabcde4dc9caf186892a803da
shehio <shehabyasser@gmail.com>
2020-05-05 10:47:54 -0700

    Attributions to be more OOP

--- src/performance/portfolioattribution.py
+++ src/performance/portfolioattribution.py
@@ -1,3 +1,5 @@
+from src.performance.assetattribution import AssetAttribution
+
 import numpy as np
 
 
@@ -14,41 +16,45 @@ class PortfolioAttribution:
         assert length == len(portfolio_returns)
         assert length == len(benchmark_returns)
 
-        self.portfolio_weights = portfolio_weights
-        self.benchmark_weights = benchmark_weights
-        self.portfolio_returns = portfolio_returns
-        self.benchmark_returns = benchmark_returns
-
         self.benchmark_total_return = 0
-        for asset_weight, asset_return in zip(self.benchmark_weights, self.benchmark_returns):
+        for asset_weight, asset_return in zip(benchmark_weights, benchmark_returns):
             self.benchmark_total_return += asset_weight * asset_return
+
+        self.asset_attributions = np.array([], dtype=AssetAttribution)
+
+        for portfolio_weight, benchmark_weight, portfolio_return, benchmark_return in \
+                zip(portfolio_weights, benchmark_weights, portfolio_returns, benchmark_returns):
+            self.asset_attributions = np.append(
+                self.asset_attributions,
+                AssetAttribution(portfolio_weight, benchmark_weight, portfolio_return, benchmark_return))
 
     def get_allocations(self):
         allocations = np.array([])
-        for portfolio_weight, benchmark_weight, benchmark_return in \
-                zip(self.portfolio_weights, self.benchmark_weights, self.benchmark_returns):
-            allocations = np.append(
-                allocations,
-                (portfolio_weight - benchmark_weight) * (benchmark_return - self.benchmark_total_return))
+        for asset_attribution in self.asset_attributions:
+            allocations = np.append(allocations, asset_attribution.get_allocation(self.benchmark_total_return))
 
         return allocations
 
     def get_selections(self):
         selections = np.array([])
-        for portfolio_return, benchmark_weight, benchmark_return in \
-                zip(self.portfolio_returns, self.benchmark_weights, self.benchmark_returns):
-            selections = np.append(
-                selections,
-                benchmark_weight * (portfolio_return - benchmark_return))
+        for asset_attribution in self.asset_attributions:
+            selections = np.append(selections, asset_attribution.get_selection())
 
         return selections
 
     def get_interactions(self):
         interactions = np.array([])
-        for benchmark_weight, benchmark_return, portfolio_weight, portfolio_return in \
-                zip(self.benchmark_weights, self.benchmark_returns, self.benchmark_weights, self.portfolio_returns):
-            interactions = np.append(
-                interactions,
-                (portfolio_weight - benchmark_weight) * (portfolio_return - benchmark_return))
+        for asset_attribution in self.asset_attributions:
+            interactions = np.append(interactions, asset_attribution.get_interaction())
 
         return interactions
+
+    def get_total_attribution(self):
+        total_attributions = np.array([])
+        for asset_attribution in self.asset_attributions:
+            total_attribution = asset_attribution.get_interaction() + \
+                                asset_attribution.get_selection() + \
+                                asset_attribution.get_interaction()
+            total_attributions = np.append(total_attributions, total_attribution)
+
+        return total_attributions
