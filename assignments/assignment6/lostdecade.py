9a0504f9be8f84a0a986296e16f08b091496eb7a
shehio <shehabyasser@gmail.com>
2020-05-16 14:02:38 -0700

    Refactoring...

---
+++ assignments/assignment6/lostdecade.py
@@ -0,0 +1,16 @@
+# The decade of the 2000s has been called the “lost decade” for the U.S. economy (Neil Irwin, “Aughts
+# were a lost decade for U.S. economy, workers,” The Washington Post, 2 January 2010). We will take a
+# look at the returns from investments in stocks and cash over this decade. You may use either R or a
+# spreadsheet to do the following analysis.
+# For the period from 2000-12-31 to 2010-12-31, form 11 portfolios of the two assets, R3000 and cash
+# (proxied by T-bills), using constant weights, ranging from 100% cash/0% equities, to 0% cash/100%
+# equities, in ten percent increments in weight. Assume the portfolios are rebalanced monthly to the fixed
+# weights (ignore transaction costs). Calculate the monthly returns of the 11 portfolios.
+# For each of the 11 portfolios, calculate the standard deviation of the returns, the arithmetic mean return,
+# the geometric mean return, and the approximate geometric mean return using the equation given in
+# Lecture 3. Plot the results on a graph (or on three graphs, if you wish), with the mean returns – arithmetic
+# and geometric – on the vertical axis, and the standard deviation of returns on the horizontal axis.
+# Comment on the accuracy of the approximate geometric mean return. Would an investment in any of the
+# fixed mix strategies have resulted in greater ending wealth than an investment in cash or stocks alone
+# over the decade? Write up your analysis in a document and submit.
+
--- assignments/assignment6/russel_returns.py
+++ assignments/assignment6/russel_returns.py
@@ -5,21 +5,27 @@ import pandas as pd
 
 from src.iohelpers import IoHelpers
 
+column_of_interest = 'Value_With_Dividends__USD_'
+
+
+def get_last_moth_data_from_russel(filename: str) -> pd.DataFrame:
+    _russel_values = IoHelpers.read_russel_returns(filename)
+    _russel_values = _russel_values[[column_of_interest]]
+    index = _russel_values.index
+    return _russel_values.loc[_russel_values.groupby(index.to_period('M')).apply(lambda x: x.index.max())]
+
 
 def get_returns_between_dates(values: pd.DataFrame, start_date: np.datetime64, end_date: np.datetime64) -> np.array:
     values = values.loc[(values.index > start_date) & (values.index <= end_date)]
-
-    returns = (values.Value_With_Dividends__USD_ / values.Value_With_Dividends__USD_.shift() - 1) * 100
+    column = values[[column_of_interest]]
+    returns = (column / column.shift() - 1) * 100
     returns = returns.to_numpy()
     returns = returns[~ np.isnan(returns)]
     return returns
 
 
 if __name__ == '__main__':
-    russel_values = IoHelpers.read_russel_returns(os.path.realpath('./../statements/valueshist_US3000_all.csv'))
-    russel_values = russel_values[['Value_With_Dividends__USD_']]
-    index = russel_values.index
-    russel_values = russel_values.loc[russel_values.groupby(index.to_period('M')).apply(lambda x: x.index.max())]
+    russel_values = get_last_moth_data_from_russel(os.path.realpath('./../statements/valueshist_US3000_all.csv'))
 
     start_date = np.datetime64(datetime.date(2000, 1, 1))
     end_date = np.datetime64(datetime.date(2009, 12, 31))
